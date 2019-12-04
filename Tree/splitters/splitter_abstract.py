34d6cfd8e2c42bec13af2956707ce129a20c9c65
afek <afek@spotlightltd.com>
2019-12-04 11:16:14 +0200

    add init to abstract class

--- Tree/splitters/splitter_abstract.py
+++ Tree/splitters/splitter_abstract.py
@@ -3,6 +3,12 @@ from numpy import array
 
 
 class Splitter:
+    def __init__(self, numeric_node, categorical_node,response_variable_type,min_samples_leaf):
+        self.numeric_node = numeric_node
+        self.categorical_node = categorical_node
+        self.type = response_variable_type
+        self.min_samples_leaf = min_samples_leaf
+
     def get_split(self, df: DataFrame):
         raise NotImplementedError
 
