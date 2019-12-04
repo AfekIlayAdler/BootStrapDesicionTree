0cfb8f60ed2af81b8fa3d5c881d14b7ca5f9e06e
afek <afek@spotlightltd.com>
2019-12-04 10:47:20 +0200

    moved method to utils

--- Tree/utils.py
+++ Tree/utils.py
@@ -2,13 +2,21 @@ from typing import Dict
 import numpy as np
 import pandas as pd
 
+from Tree.config import PANDAS_CATEGORICAL_COLS
+
 
 def get_cols_dtypes(df: pd.DataFrame, y_col_name) -> Dict:
     return {i: v.name for i, v in df.dtypes.to_dict().items() if i != y_col_name}
 
 
+def get_col_type(col_type):
+    if col_type in PANDAS_CATEGORICAL_COLS:
+        return 'categorical'
+    return 'numeric'
+
+
 def regression_impurity(y: pd.Series):
-    return np.sum(np.square(y)-np.mean(y))
+    return np.sum(np.square(y) - np.mean(y))
 
 
 def classification_impurity(y: pd.Series):
