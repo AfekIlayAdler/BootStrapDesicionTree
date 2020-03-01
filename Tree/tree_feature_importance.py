2026260a8aeb16427199aeafd376fdeee0e7544f
afek <afekilayadler@gmail.com>
2020-03-01 17:43:03 +0200

    added another variable to simplify computation

--- Tree/tree_feature_importance.py
+++ Tree/tree_feature_importance.py
@@ -2,6 +2,7 @@ from Tree.node import InternalNode
 
 
 def weighted_variance_reduction_feature_importance(tree):
+    # based on https://stats.stackexchange.com/questions/162162/relative-variable-importance-for-boosting
     # TODO: chech if bark
     queue = [[tree.root]]
     feature_importance = {feature: 0 for feature in tree.column_dtypes.keys()}
@@ -15,7 +16,9 @@ def weighted_variance_reduction_feature_importance(tree):
                 if isinstance(child, InternalNode):
                     next_level_nodes.append(child)
                 children_nodes_weighted_purity += child.purity
-            feature_importance[node.field] += (node.n_examples/tree.root.n_examples)*(node.purity/node.n_examples - children_nodes_weighted_purity/node.n_examples)
+            node_mean_purity_reduction = (node.purity - children_nodes_weighted_purity)/node.n_examples
+            # actually node.n_examples is not needed here.
+            feature_importance[node.field] += (node.n_examples/tree.root.n_examples)*node_mean_purity_reduction
         if next_level_nodes:
             queue.append(next_level_nodes)
         tree_depth += 1
