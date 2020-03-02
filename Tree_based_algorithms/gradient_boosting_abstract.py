aeb280b507556be87cac70bf088ddd9392c0ba5d
afek <afekilayadler@gmail.com>
2020-03-02 18:05:08 +0200

    change signature

--- Tree_based_algorithms/gradient_boosting_abstract.py
+++ Tree_based_algorithms/gradient_boosting_abstract.py
@@ -1,3 +1,5 @@
+from pandas import DataFrame, Series
+
 N_ESTIMATORS = 30
 LEARNING_RATE = 0.01
 GRADIENT_BOOSTING_LABEL = 'y_residual'
@@ -6,11 +8,11 @@ GRADIENT_BOOSTING_LABEL = 'y_residual'
 class GradientBoostingMachine:
     """currently supports only binomial log likelihood as in the original paper of friedman"""
 
-    def compute_gradient(self, x, y):
+    def compute_gradient(self, x: DataFrame, y: Series):
         raise NotImplementedError
 
-    def fit(self, x, y):
+    def fit(self, data: DataFrame):
         raise NotImplementedError
 
-    def predict(self, x):
+    def predict(self, data: DataFrame):
         raise NotImplementedError
