0abc063af0ea576ef4f057efc622833efa354d93
afek <afekilayadler@gmail.com>
2020-03-19 13:20:29 +0200

    added config

---
+++ experiments/detect_uninformative_feature_simulated_data/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from numpy import arange
+
+
+# exp
+CATEGORIES = arange(10, 210, 10)
+N_EXPERIMENTS = 5
+
+# data
+A1 = 3
+A2 = 2
+SIGMA = 10
+N_ROWS = 10 ** 3
+CATEGORY_COLUMN_NAME = 'random_category'
+Y_COL_NAME = 'y'
+
+
+# gbm
+MAX_DEPTH = 4
+N_ESTIMATORS = 100
+LEARNING_RATE = 0.1
+
+# io
+MODELS_DIR = Path(F"results/models/")
+RESULTS_DIR = Path(F"results/experiments_results/")
+EXP_NAME = F"detect_uninformative_feature_{MAX_DEPTH}_nestimators_{N_ESTIMATORS}_learning_rate_{LEARNING_RATE}"
---
+++ experiments/interaction/config.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from numpy import arange
+
+
+# exp
+CATEGORIES = arange(10, 200, 10)
+N_EXPERIMENTS = 2
+
+# data
+A1 = 2
+A2 = 5
+SIGMA = 3
+N_ROWS = 10**3
+VAL_RATIO = 0.15
+CATEGORY_COLUMN_NAME = 'category'
+Y_COL_NAME = 'y'
+
+# gbm
+MAX_DEPTH = 4
+N_ESTIMATORS = 100
+LEARNING_RATE = 0.1
+
+# io
+MODELS_DIR = Path(F"results/models/")
+RESULTS_DIR = Path(F"results/experiments_results/")
+EXP_NAME = F"interaction_{MAX_DEPTH}_nestimators_{N_ESTIMATORS}_learning_rate_{LEARNING_RATE}"
