b0a8714e36b02aaa03927522ed1d82d83aca0951
afek <afekilayadler@gmail.com>
2020-03-22 14:12:44 +0200

    changed default parameters

--- experiments/interaction/config.py
+++ experiments/interaction/config.py
@@ -1,16 +1,16 @@
 from pathlib import Path
 from numpy import arange
 
-
 # exp
-CATEGORIES = list(arange(2,10,2)) +list(arange(10, 200, 10))
+CATEGORIES = list(arange(2, 10, 2)) + list(arange(10, 200, 10))
+A1_VALUES = [2, 10, 50, 100, 200]
 N_EXPERIMENTS = 2
 
 # data
 A1 = 2
 A2 = 5
 SIGMA = 3
-N_ROWS = 10**3
+N_ROWS = 10 ** 3
 VAL_RATIO = 0.15
 CATEGORY_COLUMN_NAME = 'category'
 Y_COL_NAME = 'y'
